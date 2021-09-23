cc4e8bf0ad5c664c2f19f19c3c23078c3efdd699
DonovanSalgado <DonovanSalgado@DESKTOP-PBMC8M1>
2021-09-23 02:57:16 -0500

    Agregar archivos de proyecto.

---
+++ App2/App2/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App2.ViewModel
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        private string _nombre;
+
+        public MainPageViewModel()
+        {
+            _nombre = "Hola";
+        }
+
+        public string Nombre
+        {
+            get => _nombre;
+            set
+            {
+                if (string.Equals(_nombre, value)) return;
+
+                var data = _nombre.CompareTo(value);
+
+                _nombre = value;
+                RaisePropertyChanged(nameof(Nombre));
+                RaisePropertyChanged(nameof(SinUpdate));
+            }
+        }
+
+        public string SinUpdate
+        {
+            get => _nombre;
+
+        }
+       
+
+    }
+}
